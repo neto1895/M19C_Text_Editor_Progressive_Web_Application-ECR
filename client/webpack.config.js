2d418f8ddac135b41fe7aa1da633fe505112dda6
neto1895 <cruz.rosales.ernesto@gmail.com>
2023-08-06 15:59:56 -0600

    test errors to deploy

--- client/webpack.config.js
+++ client/webpack.config.js
@@ -3,9 +3,6 @@ const WebpackPwaManifest = require('webpack-pwa-manifest');
 const path = require('path');
 const { InjectManifest } = require('workbox-webpack-plugin');
 
-// TODO: Add and configure workbox plugins for a service worker and manifest file.
-// TODO: Add CSS loaders and babel to webpack.
-
 module.exports = () => {
   return {
     mode: 'development',
@@ -34,9 +31,9 @@ module.exports = () => {
       new WebpackPwaManifest({
         fingerprints: false,
         inject: true,
-        name: 'JATE JATE MAU',
+        name: 'JATE: Just Another Text Editor',
         short_name: 'JATE',
-        description: 'Ma-huaaaau!',
+        description: 'M19C_Text_Editor_Progressive_Web_Application-ECR',
         background_color: '#225ca3',
         theme_color: '#225ca3',
         start_url: './',
